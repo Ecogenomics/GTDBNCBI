7474ad61634186ee1c9b3ef5b1ff16e27a339b5d
pchaumeil <p.chaumeil@qfab.org>
2015-10-19 12:29:33 +1000

    Create Config.py

---
+++ src/gtdblite/Config.py
@@ -0,0 +1,6 @@
+GTDB_HOST="host"
+GTDB_USERNAME="dbuser"
+GTDB_DB_NAME="database_name"
+GTDB_PASSWORD="database_password"
+GTDB_GENOME_COPY_DIR="/path/to/copied_genomes/"
+GTDB_MARKER_COPY_DIR="/path/to/copied_markers/"
