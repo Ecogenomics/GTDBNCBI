08cf2c5553bf88d733bfc6d3f3eb3be603e52a84
dparks1134 <donovan.parks@gmail.com>
2015-11-02 11:09:20 +1000

    Added ConfigTemplate file.

---
+++ src/gtdblite/ConfigTemplate.py
@@ -0,0 +1,20 @@
+GTDB_HOST = "host"
+GTDB_USERNAME = "dbuser"
+GTDB_DB_NAME = "database_name"
+GTDB_PASSWORD = "database_password"
+GTDB_GENOME_COPY_DIR = "/path/to/copied_genomes/"
+GTDB_MARKER_COPY_DIR = "/path/to/copied_markers/"
+
+# Root directory for genomes
+GTDB_GENOME_ROOT = "/root/path/to/genomes/"
+
+# Deprecated folder
+GTDB_DPRCTD_ROOT = "/root/path/to/deprecated/genomes/"
+USER_SUFFIX = "suffix/for/user/genomes/"
+NCBI_SUFFIX = "suffix/for/ncbi/genomes/"
+
+GTDB_GENOME_USR_DIR = GTDB_GENOME_ROOT + USER_SUFFIX
+GTDB_GENOME_NCBI_DIR = GTDB_GENOME_ROOT + NCBI_SUFFIX
+
+GTDB_DPRCTD_USR_DIR = GTDB_DPRCTD_ROOT + USER_SUFFIX
+GTDB_DPRCTD_NCBI_DIR = GTDB_DPRCTD_ROOT + NCBI_SUFFIX
