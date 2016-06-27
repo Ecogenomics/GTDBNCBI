a92745374b8a1250f22ac02b4ef2965374dfe692
pchaumeil <uqpchaum@uq.edu.au>
2016-06-27 14:19:53 +1000

    multithread branch creation

--- src/gtdb/TreeManager.py
+++ src/gtdb/TreeManager.py
@@ -365,7 +365,7 @@ class TreeManager(object):
                                                                                                                 len(trimmed_seqs[trimmed_seqs.keys()[0]]), count_wrong_pa, count_wrong_cons))
         self.logger.info('After trimming %d taxa have amino acids in <%.1f%% of columns.' % (
             len(pruned_seqs), min_perc_aa))
-            
+
         # write out mask for MSA
         msa_mask_out = open(os.path.join(directory, prefix + "_mask.txt"), 'w')
         msa_mask_out.write(''.join(['1' if m else '0' for m in mask]))
