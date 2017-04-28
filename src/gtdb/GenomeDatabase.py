f544617a3b5a0dd9572df42864a2d2fd8d5953fc
pchaumeil <p.chaumeil@qfab.org>
2017-04-28 10:18:08 +1000

    User details.Clean code.

--- src/gtdb/GenomeDatabase.py
+++ src/gtdb/GenomeDatabase.py
@@ -173,6 +173,20 @@ class GenomeDatabase(object):
             self.conn.rollback()
             self.ReportError(e.message)
             return False
+        
+    def viewUser(self,usernames):
+        try:
+            cur = self.conn.cursor()
+            user_mngr = UserManager(cur, self.currentUser)
+            # print user details
+            header,rows = user_mngr.printUserDetails(usernames)
+            self.PrintTable(header, rows)
+        
+        except GenomeDatabaseError as e:
+            self.ReportError(e.message)
+            return False
+
+        return True
 
     def AddGenomes(self, batchfile,
                    checkm_file,
@@ -593,15 +607,6 @@ class GenomeDatabase(object):
         list
             Representative genome IDs
         """
-        print 'all:{0}'.format(all_dereplicated)
-        print 'ncbider:{0}'.format(ncbi_dereplicated)
-        print 'donovan_sra_dereplicated:{0}'.format(donovan_sra_dereplicated)
-        print 'all_genomes:{0}'.format(all_genomes)
-        print 'ncbi_genomes:{0}'.format(ncbi_genomes)
-        print 'user_genomes:{0}'.format(user_genomes)
-        print 'genome_list_ids:{0}'.format(genome_list_ids)
-        print 'genome_ids:{0}'.format(genome_ids)
-        print 'genome_batchfile:{0}'.format(genome_batchfile)
 
         genome_id_list = set()
         required_rep_genomes_ids = set()
