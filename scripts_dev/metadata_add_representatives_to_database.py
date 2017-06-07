58f6e9a1cf3c07fa9edc062afbca56a5c979a83b
dparks1134 <donovan.parks@gmail.com>
2017-06-08 02:04:01 +1000

    New development scripts.

--- scripts_dev/metadata_add_representatives_to_database.py
+++ scripts_dev/metadata_add_representatives_to_database.py
@@ -32,16 +32,77 @@ __status__ = 'Development'
 import os
 import sys
 import csv
+<<<<<<< HEAD
+import logging
+=======
+>>>>>>> d4ade230b31331bf252bfaa94230b6fdd73d3e80
 import argparse
 import tempfile
 from collections import defaultdict
 
+<<<<<<< HEAD
+from gtdb import GenomeDatabase
+from gtdb.Exceptions import (GenomeDatabaseError, 
+                                DumpDBErrors, 
+                                DumpDBWarnings, 
+                                ErrorReport)
+                                
+import psycopg2
+from psycopg2.extensions import AsIs
+
+=======
+>>>>>>> d4ade230b31331bf252bfaa94230b6fdd73d3e80
 csv.field_size_limit(sys.maxsize)
 
 class AddRepresentativeGenomes(object):
   """Populate 'gtdb_genome_representative' and 'gtdb_representative' fields in database."""
 
   def __init__(self):
+<<<<<<< HEAD
+    
+    logging.basicConfig(format="[%(asctime)s] %(levelname)s: %(message)s",
+                            datefmt="%Y-%m-%d %H:%M:%S",
+                            level=logging.DEBUG)
+    self.logger = logging
+    
+  def setup_db(self, gtdb_version):
+    """Setup database."""
+
+    # initialise the backend
+    self.db = GenomeDatabase.GenomeDatabase(1, False)
+    self.db.conn.MakePostgresConnection(gtdb_version)
+
+    # login
+    try:
+        self.db.Login(None, False)
+    except GenomeDatabaseError as e:
+        self.db.conn.ClosePostgresConnection()
+        ErrorReport(e.message + " The following error(s) were reported:\n")
+        DumpDBErrors(self.db)
+        sys.exit(-1)
+
+  def run(self, representative_file, gtdb_version):
+    """Add metadata."""
+    
+    self.setup_db(gtdb_version)
+    
+    # clear representative fields
+    cur = self.db.conn.cursor()
+        
+    q = ("UPDATE metadata_taxonomy SET gtdb_representative = NULL, gtdb_genome_representative = NULL")
+    cur.execute(q)
+    self.db.conn.commit()
+    
+    # mark all genomes as not being representatives
+    q = ("SELECT genome FROM metadata_view")
+    cur.execute(q)
+    
+    is_rep = {}
+    for r in cur:
+        is_rep[r[0]] = False
+    
+    # determine representative assignment of genomes
+=======
     pass
 
   def run(self, gtdb_metadata_file, representative_file):
@@ -60,6 +121,7 @@ class AddRepresentativeGenomes(object):
     
     # determine representative assignment of genomes
     # (very low quality genomes will not be in the representative file)
+>>>>>>> d4ade230b31331bf252bfaa94230b6fdd73d3e80
     temp_genome_rep_file = tempfile.NamedTemporaryFile(delete=False)
     for line in open(representative_file):
         line_split = line.strip().split('\t')
@@ -90,6 +152,29 @@ class AddRepresentativeGenomes(object):
     print cmd
     os.system(cmd)
     os.remove(temp_rep_file.name)
+<<<<<<< HEAD
+    
+    cur.close()
+
+if __name__ == '__main__':
+    print __prog_name__ + ' v' + __version__ + ': ' + __prog_desc__
+    print '  by ' + __author__ + ' (' + __email__ + ')' + '\n'
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('representative_file', help='representative file from genometreetk aai_cluster')
+    parser.add_argument('gtdb_version', help='GTDB database version (i.e., gtdb_releaseX)')
+
+    args = parser.parse_args()
+
+    try:
+        p = AddRepresentativeGenomes()
+        p.run(args.representative_file, args.gtdb_version)
+    except SystemExit:
+        print "\nControlled exit resulting from an unrecoverable error or warning."
+    except:
+        print "\nUnexpected error:", sys.exc_info()[0]
+        raise
+=======
 
 if __name__ == '__main__':
   print __prog_name__ + ' v' + __version__ + ': ' + __prog_desc__
@@ -109,3 +194,4 @@ if __name__ == '__main__':
   except:
     print "\nUnexpected error:", sys.exc_info()[0]
     raise
+>>>>>>> d4ade230b31331bf252bfaa94230b6fdd73d3e80
