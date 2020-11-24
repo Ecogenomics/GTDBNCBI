751e00cb5a58646755fcc9b4b5d11dcef5330c19
dparks1134 <donovan.parks@gmail.com>
2020-11-25 05:42:00 +1000

    Removed StrainInfo and DSMZ from priority date table; modified parsing of LPSN priority references to select first valid reference

--- scripts_dev/type_genome_selection/generate_date_table.py
+++ scripts_dev/type_genome_selection/generate_date_table.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 ###############################################################################
 #                                                                             #
 #    This program is free software: you can redistribute it and/or modify     #
@@ -42,34 +43,49 @@ class DateEditor(object):
     """Main class
       """
 
-    def __init__(self, lpsn_species_info, dsmz_species_info, straininfo_species_info):
+    def __init__(self):
         """Initialization."""
-        self.year = datetime.datetime.now().year
-        self.lpsn_date_dict = self.load_date_dict(lpsn_species_info)
-        self.dsmz_date_dict = self.load_date_dict(dsmz_species_info)
-        self.straininfo_date_dict = self.load_date_dict(
-            straininfo_species_info)
+        
+        pass
 
-    def load_date_dict(self, lpsn_species_info):
+    def load_date_dict(self, species_info):
+        """Parse year with priority from list of references."""
+        
+        if species_info is None:
+            return {}
+            
         datedict = {}
-        with open(lpsn_species_info) as lsi:
+        with open(species_info) as lsi:
+            lsi.readline()
             for line in lsi:
-                if 'straininfo_strains_number' in line:
-                    continue
                 infos = line.rstrip('\n').split('\t')
-                if infos[2] != '':
-                    date = re.sub(r'\([^)]*\)', '', infos[2])
-                    date = re.sub(r'emend\.[^\d]*\d{4}', '', date)
-                    date = re.sub(r' ex [^\d]*\d{4}', ' ', date)
-                    matches = re.findall('[1-3][0-9]{3}', date, re.DOTALL)
-                    matches = [int(a) for a in matches if int(a) <= self.year]
-                    if matches:
-                        datedict[infos[0].replace('s__', '')] = sorted(
-                            matches)[0]
+                
+                sp = infos[0]
+                if sp == 's__':
+                    # *** hack to skip bad case in file
+                    # Pierre to fix
+                    continue 
+
+                species_authority = infos[2]
+                reference_str = species_authority.split(', ')[0]
+                references = reference_str.replace('(', '').replace(')', '')
+                years = re.sub(r'emend\.[^\d]*\d{4}', '', references)
+                years = re.sub(r'ex [^\d]*\d{4}', ' ', years)
+                years = re.findall('[1-3][0-9]{3}', years, re.DOTALL)
+                years = [int(y) for y in years if int(y) <= datetime.datetime.now().year]
+                
+                if len(years) == 0:
+                    # assume this name is validated through ICN and just take the first 
+                    # date given as the year of priority
+                    years = re.findall('[1-3][0-9]{3}', references, re.DOTALL)
+                    years = [int(y) for y in years if int(y) <= datetime.datetime.now().year]
+                    
+                datedict[sp.replace('s__', '')] = years[0]
+                            
         # We make sure that species and subspecies type species have the same date
         # ie Photorhabdus luminescens and Photorhabdus luminescens subsp.
         # Luminescens
-        for k, v in datedict.iteritems():
+        for k, v in datedict.items():
             infos_name = k.split(' ')
             if len(infos_name) == 2 and '{0} {1} subsp. {1}'.format(infos_name[0], infos_name[1]) in datedict:
                 datedict[k] = min(int(v), int(datedict.get(
@@ -77,50 +93,49 @@ class DateEditor(object):
             elif len(infos_name) == 4 and infos_name[1] == infos_name[3] and '{} {}'.format(infos_name[0], infos_name[1]) in datedict:
                 datedict[k] = min(int(v), int(datedict.get(
                     '{} {}'.format(infos_name[0], infos_name[1]))))
+                    
         return datedict
 
-    def run(self, outfile):
+    def run(self, lpsn_species_info, outfile):
+        """Parse priority year from LPSN data."""
+        
+        self.lpsn_date_dict = self.load_date_dict(lpsn_species_info)
+        
         output_file = open(outfile, 'w')
-        all_species = self.lpsn_date_dict.keys()
-        all_species.extend(self.dsmz_date_dict.keys())
-        all_species.extend(self.straininfo_date_dict.keys())
-        for spe in set(all_species):
+        
+        for spe in self.lpsn_date_dict:
             list_date = [''] * 4
             list_date[0] = spe
             if spe in self.lpsn_date_dict:
                 list_date[1] = str(self.lpsn_date_dict.get(spe))
-            if spe in self.dsmz_date_dict:
-                list_date[2] = str(self.dsmz_date_dict.get(spe))
-            if spe in self.straininfo_date_dict:
-                list_date[3] = str(self.straininfo_date_dict.get(spe))
+
             output_file.write('{}\n'.format('\t'.join(list_date)))
+            
         output_file.close()
 
 
 if __name__ == '__main__':
-    print __prog_name__ + ' v' + __version__ + ': ' + __prog_desc__
-    print '  by ' + __author__ + ' (' + __email__ + ')' + '\n'
+    print(__prog_name__ + ' v' + __version__ + ': ' + __prog_desc__)
+    print('  by ' + __author__ + ' (' + __email__ + ')' + '\n')
 
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument(
-        '--lpsn_species_info', help='LPSN species file created by LPSN website parsing.')
-    parser.add_argument(
-        '--dsmz_species_info', help='DSMZ species file created by DSMZ website parsing.')
-    parser.add_argument(
-        '--straininfo_species_info', help='Straininfo strain file created by Straininfo website parsing.')
+    parser.add_argument('--lpsn_species_info', 
+                        help='LPSN species file created by LPSN website parsing.',
+                        required=True)
     parser.add_argument('--output',
-                        help='Output file.')
+                        help='Output file.',
+                        required=True)
 
     args = parser.parse_args()
 
     try:
-        dateeditor = DateEditor(
-            args.lpsn_species_info, args.dsmz_species_info,
-            args.straininfo_species_info)
-        dateeditor.run(args.output)
+        dateeditor = DateEditor()
+        dateeditor.run(args.lpsn_species_info,
+                        args.output)
     except SystemExit:
-        print "\nControlled exit resulting from an unrecoverable error or warning."
+        print("\nControlled exit resulting from an unrecoverable error or warning.")
+        raise
     except:
-        print "\nUnexpected error:", sys.exc_info()[0]
+        print("\nUnexpected error:", sys.exc_info()[0])
         raise
